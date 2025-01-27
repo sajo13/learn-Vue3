c53fd72c58d9de939972cac3660b9b8edeeb8209
sajo13 <sajosunny007@gmail.com>
2025-01-27 12:58:21 +0530

    Intro Deeper Look at V-Model

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -5,29 +5,24 @@ export default {
     components: {Assignment, AssignmentTags},
 
     template: `
-      <section v-show="assignments.length">
-        <h2 class="font-bold mb-2">
-          {{ title }}
-          <span>({{ assignments.length }})</span>
-        </h2>
-
-        <!--magic $event variable, contains second argument (tag) from $emit-->
-        <!--after user clicks on the tag, it emits a change with the tag selected, in turn, this component waits for that change-->
-        <!--we will the store that tag selected as the current tag-->
-        <assignment-tags 
-            :data-tags="assignments.map(a => a.tag)"
-            :current-tag="currentTag"
-            @change="currentTag = $event">
-        </assignment-tags>
-
-        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-          <assignment
-              v-for="assignment in filteredAssignments"
-              :key="assignment.id"
-              :assignment="assignment"
-          ></assignment>
-        </ul>
-      </section>
+        <section v-show="assignments.length">
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>({{ assignments.length }})</span>
+            </h2>
+            
+            <assignment-tags
+                v-model:currentTag="currentTag"
+                :initial-tags="assignments.map(a => a.tag)" 
+            ></assignment-tags>
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+               <assignment 
+                    v-for="assignment in filteredAssignments"
+                    :key="assignment.id" 
+                    :assignment="assignment"
+                ></assignment>
+            </ul>
+        </section> 
     `,
 
     props: {
