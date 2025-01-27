7b7893bbd514031c40ca37aa945f5ddeff9b5c14
sajo13 <sajosunny007@gmail.com>
2025-01-27 17:52:35 +0530

    Add ReactivityTransform macro to avoid {ref} import

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import {ref} from 'vue';
+import TheWelcome from '../components/TheWelcome.vue';
+
+// Use $ref shorthand for reactivity
+let message = $ref("Hello world");
+
+setTimeout(() => {
+  message = "I have been changed";  // Direct assignment, reactivity works
+}, 2000);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheWelcome/>
+    {{ message }} <!-- Directly using message without .value -->
+    <input v-model="message" type="text"/>
   </main>
 </template>
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,14 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    ReactivityTransform(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
