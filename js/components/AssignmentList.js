6d0d52d11d0130f98e97542e7bef49a34df110fa
sajo13 <sajosunny007@gmail.com>
2025-01-27 12:51:37 +0530

    Add Component Responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,33 +1,33 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
-    components: {Assignment},
+    components: {Assignment, AssignmentTags},
 
     template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
-            
-            <div class="flex gap-2">
-                <button
-                    @click="currentTag = tag"
-                    v-for="tag in tags" 
-                    class="border rounded px-1 py-px text-xs"
-                    :class="{
-                        'border-blue-500 text-blue-500': tag === currentTag 
-                    }"
-                >{{ tag }}</button> 
-            </div>
-            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-               <assignment 
-                    v-for="assignment in filteredAssignments"
-                    :key="assignment.id" 
-                    :assignment="assignment"
-                ></assignment>
-            </ul>
-        </section> 
+      <section v-show="assignments.length">
+        <h2 class="font-bold mb-2">
+          {{ title }}
+          <span>({{ assignments.length }})</span>
+        </h2>
+
+        <!--magic $event variable, contains second argument (tag) from $emit-->
+        <!--after user clicks on the tag, it emits a change with the tag selected, in turn, this component waits for that change-->
+        <!--we will the store that tag selected as the current tag-->
+        <assignment-tags 
+            :data-tags="assignments.map(a => a.tag)"
+            :current-tag="currentTag"
+            @change="currentTag = $event">
+        </assignment-tags>
+
+        <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+          <assignment
+              v-for="assignment in filteredAssignments"
+              :key="assignment.id"
+              :assignment="assignment"
+          ></assignment>
+        </ul>
+      </section>
     `,
 
     props: {
@@ -48,10 +48,6 @@ export default {
             }
 
             return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
