706ccab2f0906fb403ac60dc400ce8018a7b674b
sajo13 <sajosunny007@gmail.com>
2025-01-27 13:22:05 +0530

    Intro More Flexible Components
    - With Slots and Flags.

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -5,11 +5,15 @@ export default {
     components: {Assignment, AssignmentTags},
 
     template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
+        <section v-show="assignments.length" class="w-60">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
             
             <assignment-tags
                 v-model:currentTag="currentTag"
@@ -22,17 +26,20 @@ export default {
                     :assignment="assignment"
                 ></assignment>
             </ul>
+            
+            <slot></slot>
         </section> 
     `,
 
     props: {
         assignments: Array,
-        title: String
+        title: String,
+        canToggle: {type: Boolean, default: false}
     },
 
     data() {
         return {
-            currentTag: 'all'
+            currentTag: 'all',
         };
     },
 
