a30e0cd7dd37d6ba494264701e26af70e6358196
sajo13 <sajosunny007@gmail.com>
2025-01-28 12:43:35 +0530

    Add Dashboard of managing Team
    - Added Components for Member list, Header, footer.
    - Added Teams Route to Head Section.

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css'
 
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
+import {createApp} from "vue";
 import {createPinia} from "pinia";
+import App from "./App.vue";
+import router from "./router";
+
+import './assets/index.css'
 
 const app = createApp(App)
 
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,13 +4,15 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     ReactivityTransform(),
-    vueDevTools()
+    vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
