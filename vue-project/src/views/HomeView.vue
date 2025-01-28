85aa13783dca9fd0e7e4c03493e965d5bf0e340c
sajo13 <sajosunny007@gmail.com>
2025-01-28 11:00:00 +0530

    Store State in an External File

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,14 +1,9 @@
-<script setup>
-import TabbableTextarea from "@/components/TabbableTextarea.vue";
-import {ref} from "vue";
-
-let comment = ref('initial textarea value');
-</script>
-
 <template>
-  <main>
-    <form>
-      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;"/>
-    </form>
-  </main>
+  <div>
+    <h5>{{ state.name }}</h5>
+  </div>
 </template>
+
+<script setup>
+import {state} from "@/stores/quizStore";
+</script>
